93302953dae285585afe583710d14c8ea0ab7aec
Nisvet Mujkic <nisvet.mujkic@edu.fit.ba>
2018-01-14 22:13:42 +0100

    Zadatak

---
+++ RS1_Ispit_2017-06-21_v1/RS1_Ispit_2017-06-21_v1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RS1_Ispit_2017_06_21_v1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
