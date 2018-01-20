581976e06fa77e3d1de1aa69ed3ac2fded0e4f30
Nisvet Mujkic <nisvet.mujkic@edu.fit.ba>
2018-01-20 10:38:33 +0100

    wer

--- RS1_PrakticniDioIspita_2017_01_24_RadnaVerzija/RS1_PrakticniDioIspita_2017_01_24/Controllers/OdrzaniCasoviController.cs
+++ RS1_PrakticniDioIspita_2017_01_24_RadnaVerzija/RS1_PrakticniDioIspita_2017_01_24/Controllers/OdrzaniCasoviController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RS1_PrakticniDioIspita_2017_01_24.EF;
 using RS1_PrakticniDioIspita_2017_01_24.ViewModels;
 
@@ -27,15 +28,10 @@ namespace RS1_PrakticniDioIspita_2017_01_24.Controllers
             {
                 Rows = db.OdrzaniCasDetalji.Select(x => new OdrzaniCasoviIndexViewModel.Row()
                 {
-                    OdrzaniCasDetaljiId = x.Id,
-                    Datum = x.OdrzaniCas.datum.ToShortDateString(),
-                    Ocjena = x.Ocjena,
-                    Odjeljenje = x.OdrzaniCas.Angazovan.Odjeljenje.Oznaka,
-                    OpravdanoOdsutan = x.OpravdanoOdsutan.Value,
-                    Odsutan = x.Odsutan,
-                    Predmet = x.OdrzaniCas.Angazovan.Predmet.Naziv,
-                    UkupnoUcenika = db.OdrzaniCasDetalji.Where(k => k.OdrzaniCasId == x.OdrzaniCasId).Count(),
-                    UkupnoPrisutnih = db.OdrzaniCasDetalji.Count(k => k.OdrzaniCasId == x.OdrzaniCasId && !k.Odsutan)
+                    OdrzaniCasDetaljId = x.Id,
+                    Datum = x.OdrzaniCas.datum,
+                    Odjeljenje = x.UpisUOdjeljenje.Odjeljenje.Oznaka,
+                    Predmet = x.OdrzaniCas.Angazovan.Predmet.Naziv
                 }).ToList()
             };
 
--- RS1_PrakticniDioIspita_2017_01_24_RadnaVerzija/RS1_PrakticniDioIspita_2017_01_24/ViewModels/OdrzaniCasoviIndexViewModel.cs
+++ RS1_PrakticniDioIspita_2017_01_24_RadnaVerzija/RS1_PrakticniDioIspita_2017_01_24/ViewModels/OdrzaniCasoviIndexViewModel.cs
@@ -7,19 +7,15 @@ namespace RS1_PrakticniDioIspita_2017_01_24.ViewModels
 {
     public class OdrzaniCasoviIndexViewModel
     {
-        public List<Row> Rows { get; set; }
+        public List<Row> Rows{ get; set; }
 
         public class Row
         {
-            public int OdrzaniCasDetaljiId { get; set; }
-            public string Datum { get; set; }
+            public int OdrzaniCasDetaljId { get; set; }
+            public DateTime Datum { get; set; }
             public string Odjeljenje { get; set; }
             public string Predmet { get; set; }
-            public int? Ocjena { get; set; }
-            public bool Odsutan { get; set; }
-            public bool OpravdanoOdsutan { get; set; }
-            public int UkupnoUcenika { get; set; }
-            public int UkupnoPrisutnih { get; set; }
+            public int MyProperty { get; set; }
         }
     }
 }
